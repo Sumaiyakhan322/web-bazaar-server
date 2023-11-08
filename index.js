5f3ed46d751bdfe00e9d8ae4111b8f340524c85e
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2023-11-08 18:47:08 +0600

    added the jwt and cors

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const express = require('express');
+const express = require("express");
 const jwt = require("jsonwebtoken");
-const cors = require('cors');
+const cors = require("cors");
 var cookieParser = require("cookie-parser");
-const app=express()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-const port =process.env.PORT  || 5000
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 //middle ware
-app.use(cors({
-  origin: [
-   
-    "https://webbazaar-creations.web.app",
-    "https://webbazaar-creations.firebaseapp.com",
-    'http://localhost:5173'
-      
-      
-  ],
-  credentials: true,
-}
-
-))
-app.use(express.json())
+app.use(
+  cors(
+    {
+    origin: [
+      "https://webbazaar-creations.web.app",
+      "https://webbazaar-creations.firebaseapp.com",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  }
+  )
+);
+app.use(express.json());
 app.use(cookieParser());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ej2tmfe.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -33,7 +30,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -42,11 +39,11 @@ async function run() {
     // await client.connect();
 
     //make a db collection
-    const jobsCollection=client.db('JobsDb').collection("Jobs");
-    const userBidsCollection=client.db('JobsDb').collection('usersBid');
-    
+    const jobsCollection = client.db("JobsDb").collection("Jobs");
+    const userBidsCollection = client.db("JobsDb").collection("usersBid");
+
     //auth route
-    app.post('/jwt',async(req,res)=>{
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
         expiresIn: "1000h",
@@ -60,66 +57,120 @@ async function run() {
         })
         .send({ success: true });
     });
-  
-    //add jobs to mongodb
-    app.post('/addJobs',async(req,res)=>{
-      const newJob=req.body;
-      const result=await jobsCollection.insertOne(newJob);
-      res.send(result);
-    })
-    //get all the jobs
-    app.get('/addJobs',async(req,res)=>{
-      const cursor=jobsCollection.find();
-      const result=await cursor.toArray();
-      res.send(result)
-    })
-    //get a job
-    app.get('/addJobs/:id',async(req,res)=>{
-      const id=req.params.id
-      const query={_id:new ObjectId(id)}
-      const result =await jobsCollection.findOne(query);
-     
-     res.send(result)
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
 
-    })
+    //add jobs to mongodb
+    app.post("/addJobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
+    });
+    //get all the jobs
+    app.get("/addJobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //get a job
+    app.get("/addJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
     //delete a job
-    app.delete('/addJobs/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const result=await jobsCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/addJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    });
     //update a job
-    app.put('/addJobs/:id',async(req,res)=>{
-      const id=req.params.id;
-      const filter={_id:new ObjectId(id)}
-      const options={upset:true}
-      const updateJob=req.body;
-      const job={
-        $set:{
-         email:updateJob.email,
-         jobTile:updateJob.jobTile,
-        img:updateJob.img,
-        deadline:updateJob.deadline ,
-         category:updateJob.category ,
-         minimum:updateJob.minimum ,
-         maximum:updateJob.maximum ,
-         des:updateJob.des ,
-        }
-      }
-      const result=await jobsCollection.updateOne(filter,job,options)
-      res.send(result)
-    })
+    app.put("/addJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upset: true };
+      const updateJob = req.body;
+      const job = {
+        $set: {
+          email: updateJob.email,
+          jobTile: updateJob.jobTile,
+          img: updateJob.img,
+          deadline: updateJob.deadline,
+          category: updateJob.category,
+          minimum: updateJob.minimum,
+          maximum: updateJob.maximum,
+          des: updateJob.des,
+        },
+      };
+      const result = await jobsCollection.updateOne(filter, job, options);
+      res.send(result);
+    });
 
     //post user bid data to mongo
-    app.post('/usersBids',async(req,res)=>{
-      const newBids=req.body;
-      const result =await userBidsCollection.insertOne(newBids)
-      res.send(result)
-    })
+    app.post("/usersBids", async (req, res) => {
+      const newBids = req.body;
+      const result = await userBidsCollection.insertOne(newBids);
+      res.send(result);
+    });
+
+    //get bid
+    app.get("/usersBids", async (req, res) => {
+      const user = req.query;
+   
+      var [keys] = Object.keys(user);
+
+      if (keys == "userEmail") {
+        const email = req.query.userEmail;
+        const query = { userEmail: email };
+        const cursor = userBidsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } else if (keys == "buyerEmail") {
+        const email = req.query.buyerEmail;
+        const query = { buyerEmail: email };
+        const cursor = userBidsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        const cursor = userBidsCollection.find().sort({status:1});
+
+        const result = await cursor.toArray();
+
+        res.send(result);
+      }
+    });
+    //get a bid by id
+    app.get("/usersBids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userBidsCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+    app.patch("/usersBids/:id", async (req, res) => {
+      const updatedBids = req.body;
+      const id = req.params.id;
+      console.log(id);
+
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: updatedBids.status,
+          disable:updatedBids.disable
+        },
+      };
+      const result = await userBidsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -127,12 +178,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req,res)=>{
-    res.send("Web-bazaar is running")
-
-})
-app.listen(port, ()=>{
-    console.log("Server is running");
-})
+app.get("/", (req, res) => {
+  res.send("Web-bazaar is running");
+});
+app.listen(port, () => {
+  console.log("Server is running");
+});
